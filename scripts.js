f83f78811b92411c61c412454d479da17939a84d
arpado <ozsvar.arpad@gmail.com>
2022-01-05 18:40:05 +0100

    extra comments added

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ let createNewSentButton = document.querySelector('.new');
 let inputSentence = document.querySelector('#input-sentence');
 let wordSelected = '';
 
-let buttonContent = ['red', 'blue', 'green'];
-let numberOfButtons = 6;
+//container a gombok szamara
 let buttonContainer = document.createElement('div');
 buttonContainer.setAttribute('class', 'button-container');
 
+let buttonContent = ['red', 'blue', 'green'];
+let numberOfButtons = 6;
 
+// legordulo gombsavot letrehozza
 function makeButtons(locX, locY, buttonContent) {
     container.appendChild(buttonContainer);
     buttonContainer.style.left = locX + 'px';
@@ -23,6 +25,7 @@ function makeButtons(locX, locY, buttonContent) {
     }
 }
 
+// a kijelolt szo (amit a gombokkal valtoztat) jelolesenek torlese
 function clearSelectedWord() {
     if (document.querySelector('#wordSelected')) {
         wordSelected = document.querySelector('#wordSelected');
@@ -30,14 +33,16 @@ function clearSelectedWord() {
     }
 }
 
+// bezarja a gombsavot
 function closeButtonMenu() {
     buttonContainer.innerHTML = '';
     buttonContainer.remove();
 }
 
+// letrehozza a foreszben a mondatszerkezetet, ezt a reszt jelentosen atirom
 createNewSentButton.addEventListener('click', () => {
-    if (inputSentence.value === '') {
-        return;
+    if (inputSentence.value.trim() === '') {
+        return inputSentence.value = '';
     }
 
     let sentenceDiv = document.createElement('div');
@@ -49,40 +54,56 @@ createNewSentButton.addEventListener('click', () => {
     sentenceIndicatorDiv.setAttribute('class', 'sentence-indicator');
 
     let inputSentenceSplit = inputSentence.value.trim().split(' ');
+    let tryOut = document.createElement('div');
+    tryOut.setAttribute('class', 'try-out')
+    sentenceDiv.append(tryOut);
 
     for (let i = 0; i < inputSentenceSplit.length; i++) {
         //lehet, h a wordcontainer nem is kell majd
-        let wordDivContainer = document.createElement('div');
+        /*let wordDivContainer = document.createElement('div');
         sentenceDiv.append(wordDivContainer);
-        wordDivContainer.setAttribute('class', 'word-container');
+        wordDivContainer.setAttribute('class', 'word-container');*/
 
         let wordDiv = document.createElement('div');
-        wordDivContainer.append(wordDiv);
+        sentenceDiv.append(wordDiv);
         wordDiv.setAttribute('class', 'individual-word');
-        wordDiv.innerText = inputSentenceSplit[i] + ' ';
+        wordDiv.innerText = inputSentenceSplit[i];
+        let spaceBetween = document.createElement('div');
+        sentenceDiv.append(spaceBetween);
+        spaceBetween.setAttribute('class', 'space-between')
+        spaceBetween.innerText = ' ';
     }
 
     inputSentence.value = '';
 });
 
+// a majdnem teljes oldalon talalhato kattra figyel, kivetel a beviteli mezo es a hozzatartozo gomb
 document.addEventListener('click', e => {
 
+    //ha meg lenne nyitva a gombsav, akkor lecsukja eloszor
     if (buttonContainer) {
         closeButtonMenu();
     }
 
+    // ha a hasznalo valid szora kattint, megnyitja a gombsavot
     if (e.target.classList.contains('individual-word')) {
         // ez a clearSelect csak biztonsagbol van itt, elvileg redundans
         clearSelectedWord();
         makeButtons(e.pageX, e.pageY, buttonContent);
         e.target.setAttribute('id', 'wordSelected');
-    } else if (e.target.classList.contains('button')) {
+    } 
+    
+    // ha a gombra kattint valtoztatasok, majd torli a szelekciot
+    else if (e.target.classList.contains('button')) {
         wordSelected = document.querySelector('#wordSelected');
         let color = e.target.id;
         wordSelected.style.background = color;
         clearSelectedWord();
         closeButtonMenu();
-    } else {
+    } 
+    
+    // invalid katt, torli a jelolest, lecsukja a gombsavot
+    else {
         clearSelectedWord();
         closeButtonMenu();
     }
