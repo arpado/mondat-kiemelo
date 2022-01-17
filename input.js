54d2ab3282a80739cd7ab19d476d6ed2c772f50b
arpado <ozsvar.arpad@gmail.com>
2022-01-17 23:50:17 +0100

    lorem & clear buttons added, another print button added, which in addition closes the menu on click

--- input.js
+++ input.js
@@ -6,8 +6,10 @@ let inputBtnBox = document.querySelector('#input-btn-div')
 
 //ezekre lehet, h nincs is szukseg
 let createNewSentButton = document.querySelector('#add-new-sentence');
+//let createNewSentAndCloseBtn = document.querySelector('#add-new-sentence-and-close');')
 let generateLipsumBtn = document.querySelector('#lipsum');
-let generateDividedLipsumBtn = document.querySelector('#divided-lipsum')
+let generateDividedLipsumBtn = document.querySelector('#divided-lipsum');
+//let
 
 // letrehozza a foreszben a mondatszerkezetet, lipsumot general - gombok. ezt a reszt jelentosen atirom
 inputBtnBox.addEventListener('click', (e) => {
@@ -15,14 +17,22 @@ inputBtnBox.addEventListener('click', (e) => {
         case 'add-new-sentence':
             addNewSentence();
             break;
+        case 'add-new-sentence-and-close':
+            addNewSentence();
+            closeMenuRemoveShine(inputWindow, toggleInputBtn);
+            break;
         case 'lipsum':
             generateLipsum();
             break;
         case 'divided-lipsum':
             generateDividedLipsum();
             break;
+        case 'clear-input':
+            clearInput();
+            break;
     }
 });
+let sentenceCounter = 1;
 
 function addNewSentence() {
 
@@ -46,19 +56,32 @@ function addNewSentence() {
     let rankDiv = document.createElement('div');
     sentenceDiv.append(rankDiv);
     rankDiv.setAttribute('class', 'rank-box');
+
     let upperRankDiv = document.createElement('div');
     rankDiv.append(upperRankDiv);
-    upperRankDiv.innerText = i + 1;
+    //ID NEM JO - csekkolni, valszeg jo
+    upperRankDiv.innerHTML = `<input type="number" name="sentence-rank" class="number-input sentence-counter" id="sentence-rank-${sentenceCounter}">`;
+    sentenceCounter++;
+
     let arrow = document.createElement('div');
     rankDiv.append(arrow);
-    arrow.innerText = '->'
+    arrow.innerHTML = '<i class="fas fa-chevron-up"></i>'
+
     let lowerRankDiv = document.createElement('div');
     rankDiv.append(lowerRankDiv);
-    lowerRankDiv.innerText = 'Later'
+    lowerRankDiv.innerText = i + 1;
+
+    toggleBorder(rankingBoxCollection, 'rankingBoxBorderToggle', 'rankingBoxBorderValueInput', 'rankingBoxBorderUnitInput', 'rankingBoxBorderStyleInput', 'rankingBoxBorderColorInput');
+    if (localStorage.getItem('rankingBoxToggle') === '0') {
+        rankDiv.classList.add('hidden');
+    }
 
     let subSentenceDiv = document.createElement('div');
     sentenceDiv.append(subSentenceDiv);
     subSentenceDiv.setAttribute('class', 'sentence-box');
+    if (localStorage.getItem('sentenceBoxBorderToggle') === '1') {
+        subSentenceDiv.style.border = `${localStorage.getItem('sentenceBoxBorderValueInput')}${localStorage.getItem('sentenceBoxBorderUnitInput')} ${localStorage.getItem('sentenceBoxBorderStyleInput')} ${localStorage.getItem('sentenceBoxBorderColorInput')}`;
+    }
 
     // ki
     /*let sentenceIndicatorDiv = document.createElement('div');
@@ -81,12 +104,13 @@ function addNewSentence() {
         wordDivContainer.setAttribute('class', 'word-container');*/
         if (inputSentenceSplit[j] !== '') {
 
-        let wordDiv = document.createElement('div');
-        subSentenceDiv.append(wordDiv);
-        wordDiv.setAttribute('class', 'individual-word');
-        wordDiv.innerText = inputSentenceSplit[j].trim();
-        wordDiv.style.border = `${localStorage.getItem('wordBoxBorderValueInput')}${localStorage.getItem('wordBoxBorderUnitInput')} ${localStorage.getItem('wordBoxBorderStyleInput')} ${localStorage.getItem('wordBoxBorderColorInput')}`;
-        
+            let wordDiv = document.createElement('div');
+            subSentenceDiv.append(wordDiv);
+            wordDiv.setAttribute('class', 'individual-word');
+            wordDiv.innerText = inputSentenceSplit[j].trim();
+            if (localStorage.getItem('wordBoxBorderToggle') === '1') {
+                wordDiv.style.border = `${localStorage.getItem('wordBoxBorderValueInput')}${localStorage.getItem('wordBoxBorderUnitInput')} ${localStorage.getItem('wordBoxBorderStyleInput')} ${localStorage.getItem('wordBoxBorderColorInput')}`;
+            }
         }
         //ki
         /*let spaceBetween = document.createElement('div');
@@ -105,4 +129,8 @@ function generateLipsum() {
 
 function generateDividedLipsum() {
     inputSentence.value = 'Lorem ipsum dolor sit,# amet consectetur adipisicing elit, illo voluptatibus,# quibusdam nulla tempore velit cumque eaque quia, autem iure itaque repellat,# quo quaerat in distinctio fugit provident voluptate eveniet dolores?'
+}
+
+function clearInput() {
+    inputSentence.value = '';
 }
