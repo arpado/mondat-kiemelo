9b9f7c47746f598a39cfbb0c25c2c581c9d51174
arpado <ozsvar.arpad@gmail.com>
2022-01-21 18:01:27 +0100

    init commit, this is the proto object for the inputs

---
+++ InputProto.js
@@ -0,0 +1,21 @@
+class Input {
+    constructor(name, node) {
+        this.name = name,
+        this.node = document.querySelector(node)
+    }
+
+    intoLS() {
+        localStorage.setItem(this.name, this.node.value);
+    }
+
+    fromLS() {
+        return localStorage.getItem(this.name);
+    }
+
+    setValue() {
+        this.node.value = localStorage.getItem(this.name);
+    }
+
+}
+
+let something = new Input();
